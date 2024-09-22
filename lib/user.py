5fe79938fab286cee7da749dfec5226a5fb49251
Your Name <princesslorna2000@gmail.com>
2024-09-22 13:03:13 +0300

    new

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        # Initialize first_name and last_name attributes
+        self.first_name = first_name
+        self.last_name = last_name
